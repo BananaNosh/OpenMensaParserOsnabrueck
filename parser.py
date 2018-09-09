6cdd6bb404454e2e3a95e126155ee77f50748486
Jonathan Loos <jonathan.loos@web.de>
2018-09-09 20:19:40 +0200

    Add xml feed builder and finalize

--- parser.py
+++ parser.py
@@ -10,36 +10,34 @@ class UnexpectedFormatError(AttributeError):
     pass
 
 
+CATEGORY_KEY = "cat"
 MAIN_MEAL_KEY = "mm"
 ADDITION_KEY = "a"
 PRICE_KEY = "p"
 DATE_KEY = "d"
-STUDENT_KEY = "stud"
-EMPLOYEE_KEY = "bed"
-
-canteen = op.LazyBuilder()
-search_date = datetime.datetime(2018, 9, 7)  # TODO
+STUDENT_KEY = "student"
+EMPLOYEE_KEY = "employee"
+MENSAE = ["westerberg", "mschlossg", "mhaste", "mvechta"]
 
 
-def get_meals(date=None):
-    result = requests.get("https://osnabrueck.my-mensa.de/essen.php?v=5121119&hyp=1&lang=de&mensa=westerberg")
+def get_meals(_mensa, date=None):
+    result = requests.get(f"https://osnabrueck.my-mensa.de/essen.php?v=5121119&hyp=1&lang=de&mensa={_mensa}")
     if result.status_code == 200:
         content = result.content
     else:
         raise ConnectionError
     b_soup = BeautifulSoup(content, "lxml")
     print(b_soup.prettify())
-    # TODO change mensa name and year
     unparsed_meals = b_soup.find_all(
-        href=lambda href: href and re.compile("mensa=westerberg#westerberg_tag_20\d{3,5}_essen").search(href))
-    print(unparsed_meals)
-    meals = []
+        href=lambda href: href and re.compile(f"mensa={_mensa}#{_mensa}_tag_20\d{{3,5}}_essen").search(href))
+    _meals = []
     for meal in unparsed_meals:
+        category = meal.parent.previous_sibling.previous_sibling.text
         meal_info = meal.find_all(["h3", "p"])
         if len(meal_info) != 3:
             raise UnexpectedFormatError("More than 3 meal info")
         meal_info = [unicodedata.normalize("NFKD", info.text).replace("\xad", "") for info in meal_info]
-        main_meal, additional, price = meal_info
+        _main_meal, _additional, price = meal_info
         price_search = re.compile("((\d+,\d{2})|-)\D*((\d+,\d{2})|-)").search(price)
         if not price_search:
             raise UnexpectedFormatError(f"price formation error {price}")
@@ -61,10 +59,28 @@ def get_meals(date=None):
             date_days = (date - datetime.datetime(date.year, 1, 1)).days
             if date_days != day or year != date.year:
                 continue
-        date = datetime.datetime(year, 1, 1) + datetime.timedelta(day)
-        meals.append({MAIN_MEAL_KEY: main_meal, ADDITION_KEY: additional, PRICE_KEY: price, DATE_KEY: date})
-    return meals
+        meal_date = datetime.datetime(year, 1, 1) + datetime.timedelta(day)
+        _meals.append({CATEGORY_KEY: category, MAIN_MEAL_KEY: _main_meal,
+                      ADDITION_KEY: _additional, PRICE_KEY: price, DATE_KEY: meal_date.date()})
+    return _meals
 
 
-print(get_meals())
-
+if __name__ == '__main__':
+    for mensa in MENSAE[:1]:
+        canteen = op.LazyBuilder()
+        meals = get_meals(mensa)
+        for meal in meals:
+            main_meal = meal[MAIN_MEAL_KEY]
+            additional = meal[ADDITION_KEY]
+            ing_reg = re.compile("\(((\d+|[a-n])(,(\d+|[a-n]))*)\)")
+            ingredients_match = ing_reg.findall(main_meal + " " + additional)
+            ingredients = list(set(",".join([ingred[0] for ingred in ingredients_match]).split(",")))
+            ingredients.sort()
+            ingredients = ",".join(ingredients)
+            main_meal = ing_reg.sub("", main_meal)
+            additional = ing_reg.sub("", additional)
+            notes = [note for note in [additional, ingredients] if len(note) > 0]
+            prices = {role: price for role, price in meal[PRICE_KEY].items() if price}
+            canteen.addMeal(meal[DATE_KEY], meal[CATEGORY_KEY], main_meal,
+                            notes if len(notes) > 0 else None, prices)
+        print(canteen.toXMLFeed())
